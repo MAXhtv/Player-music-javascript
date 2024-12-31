28a9803b85e0844f3d6ccb382a099501c000d753
MAXhtv <m4th3usv11ct0r@gmail.com>
2024-12-31 14:54:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById("audio");
+const cover = document.getElementById("cover");
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
+
+const Musica1 = {
+  songName: 'Yeah Motherfucker',
+  file: 'bah',
+  artist: 'Me Hahaha'
+};
+
+const Musica2 = {
+  songName: 'Daaaamn',
+  file: 'bah2',
+  artist: 'Me Hahaha'
+};
+
+const playlist = [Musica1, Musica2];
+let index = 0;
+let isPlaying = false;
+
+function initSong() {
+  const currentSong = playlist[index];
+  cover.src = `/imagens/${currentSong.file}.jpg`;
+  songName.innerText = currentSong.songName;
+  bandName.innerText = currentSong.artist;
+  song.src = `/songs/${currentSong.file}.mp3`;
+}
+
+function playSong() {
+  play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+  play.querySelector(".bi").classList.add("bi-pause-circle-fill");
+  song.play();
+  isPlaying = true;
+}
+
+function pauseSong() {
+  play.querySelector(".bi").classList.add("bi-play-circle-fill");
+  play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+  song.pause();
+  isPlaying = false;
+}
+
+function playPauseDecider() {
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+}
+
+function previousSong() {
+  if (index === 0) {
+    index = playlist.length - 1;
+  } else {
+    index -= 1;
+  }
+  initSong();
+  playSong();
+}
+
+function nextSong() {
+  if (index === playlist.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  initSong();
+  playSong();
+}
+
+// Event Listeners
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+
+// Initialize
+initSong();
